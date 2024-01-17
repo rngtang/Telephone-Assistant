23b4b9b6d380b8724f09ea928c16f207d23f224b
project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227 <project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227@gitlab.oit.duke.edu>
2024-01-17 10:16:27 -0500

    audio is recorded but with static noice

--- sdtest/recording_test.py
+++ sdtest/recording_test.py
@@ -1,11 +1,13 @@
 import sounddevice
 from scipy.io.wavfile import write
-sr = 48000
+import wavio as wv
+
+sr = 44100
 seconds = 5
 
 print("Recording")
-record_voice = sounddevice.rec(sr*seconds, samplerate=sr, channels=2)
+record_voice = sounddevice.rec(int(sr*seconds), samplerate=sr, channels=2, dtype='float32')
 sounddevice.wait()
 
-write("test_audio.wav", sr, record_voice)
+wv.write("test_audio.wav", record_voice, sr, sampwidth=2)
 print("Finished")
