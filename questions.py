f348397749d5ead32eca97358dd55f3e0115938c
project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227 <project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227@gitlab.oit.duke.edu>
2024-01-19 10:55:55 -0500

    should identify if it is a question

--- questions.py
+++ questions.py
@@ -1,4 +1,4 @@
-# I'm trying to crete a script that recognizes a wake up word
+# I'm trying to crete a script that recognizes a question
 
 import os
 import azure.cognitiveservices.speech as speechsdk
@@ -7,8 +7,12 @@ import azure.cognitiveservices.speech as speechsdk
 speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('COLAB_SPEECH_KEY'), region=os.environ.get('COLAB_SPEECH_REGION'))
 speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
 
-print("Ask me a question")
+question_words = ["what", "who", "where", "when", "why", "how", "whose", "which", "?"]
 
-# Result
-result = speech_recognizer.recognize_once()
-print(result)
+print("Hello, how can I help you?")
+
+# Gets listens
+result = speech_recognizer.recognize_once().text
+# checks if it is a question
+q = any(i in result for i in question_words)
+print(q)
