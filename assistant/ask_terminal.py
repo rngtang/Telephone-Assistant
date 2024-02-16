fcb6c1d175beafd05cc1eb17760b2d2aacb6c47a
project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227 <project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227@gitlab.oit.duke.edu>
2024-02-16 11:09:57 -0500

    assistant works with terminal input

--- assistant/ask_terminal.py
+++ assistant/ask_terminal.py
@@ -3,51 +3,52 @@ import time
 client = OpenAI()
 
 assistant_id = "asst_ohl8sJLMkCw6K6xN0iUrFuwJ"
+print("Hello, how can I help you?")
 
-# Creates a thread and adds a message
-message_thread = client.beta.threads.create(
-  messages=[
-    {
-        "role": "user",
-        "content": "What is the colab?"
-    }
-  ]
-)
-
-# print("Assistant id: {id}".format(id=assistant_id))
-
+# Creates a thread
+message_thread = client.beta.threads.create()
 thread_id = message_thread.id
-# print("Thread id: {id}".format(id=thread_id))
 
-# Starts a run
-start_run = client.beta.threads.runs.create(
-  thread_id=thread_id,
-  assistant_id=assistant_id,
-  instructions="Answer the questions."
-)
-
-run_id = start_run.id
-
-# Waits until gets the response
 while True:
-    run_status = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run_id)
-    if run_status.status == "completed":
-        break
-    elif run_status.status == "failed":
-        print("Run failed:", run_status.last_error)
-        break
-    time.sleep(2)  
+    question = input("Ask me anything: ")
 
-# Retrives the messages
-messages = client.beta.threads.messages.list(
-  thread_id=thread_id
-)
+    client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=question
+    )
 
-# Prints the messages
-for i in range(1,3):
-    message = messages.data[len(messages.data)-i]
-    role = message.role
-    for content in message.content:
-        if content.type == 'text':
-            response = content.text.value 
-            print(f'\n{role}: {response}')
+    # Starts a run
+    start_run = client.beta.threads.runs.create(
+    thread_id=thread_id,
+    assistant_id=assistant_id,
+    instructions="Answer the questions."
+    )
+
+    run_id = start_run.id
+
+    # Waits until gets the response
+    print("Thinking...")
+    while True:
+        run_status = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run_id)
+        if run_status.status == "completed":
+            break
+        elif run_status.status == "failed":
+            print("Run failed:", run_status.last_error)
+            break
+        time.sleep(2)  
+
+    # Retrives the messages
+    messages = client.beta.threads.messages.list(
+    thread_id=thread_id
+    )
+
+    # Prints the messages
+    for i in reversed(range(0,2)):
+        message = messages.data[i]
+        role = message.role
+        for content in message.content:
+            if content.type == 'text':
+                response = content.text.value 
+                print(f'\n{role}: {response}')
+    print("\n")
