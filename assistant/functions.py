94d03eec97356cb45e994b2ab1f5aeb2ec802c44
project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227 <project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227@gitlab.oit.duke.edu>
2024-02-19 16:00:07 -0500

    creating smaller subfunctions

--- assistant/functions.py
+++ assistant/functions.py
@@ -11,6 +11,36 @@ print("Hello, how can I help you?")
 message_thread = client.beta.threads.create()
 thread_id = message_thread.id
 
+
+# create smaller subfunctions for use: https://cookbook.openai.com/examples/assistants_api_overview_python 
+def wait_on_run(run, thread):
+    while run.status == "queued" or run.status == "in_progress":
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
+        )
+        time.sleep(0.5)
+    return run
+
+def get_response(thread):
+    return client.beta.threads.messages.list(thread_id=thread.id, order="asc")
+
+def submit_message(assistant_id, thread, user_message):
+    client.beta.threads.messages.create(
+        thread_id=thread.id, role="user", content=user_message
+    )
+    return client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant_id,
+    )
+
+def create_thread_and_run(user_input):
+    thread = client.beta.threads.create()
+    run = submit_message(assistant_id, thread, user_input)
+    return thread, run
+
+
+
 while True:
     question = input("Ask me anything: ")
 
@@ -58,6 +88,11 @@ while True:
                         }
                     ],
                 )
+
+                run = wait_on_run(run, thread)
+                print(get_response(thread))
+
+
 
                 break
             except: 
