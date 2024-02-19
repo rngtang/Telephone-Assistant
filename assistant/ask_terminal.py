3312ed6dbd59c5eab008a1b85bbcb444235e4bf9
project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227 <project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227@gitlab.oit.duke.edu>
2024-02-19 15:53:31 -0500

    can call function but nothing returned

--- assistant/ask_terminal.py
+++ assistant/ask_terminal.py
@@ -22,7 +22,7 @@ while True:
     start_run = client.beta.threads.runs.create(
     thread_id=thread_id,
     assistant_id=assistant_id,
-    instructions="Answer the questions."
+    instructions="You are the assistant of Duke's Innovation Colab. Your main role is to assist students in answering their questions. Most of the students come for help to use tools like 3d printers, laser cutters, or software. All the information that you need is the document provided. You should avoid answering questions that don't have a relationship with the Colab or its facilities. If someone asks anything that is not related to the colab, you should respond that you are not able to answer their questions"
     )
 
     run_id = start_run.id
---
+++ assistant/functions.py
@@ -0,0 +1,90 @@
+from openai import OpenAI
+import time
+import json
+
+client = OpenAI()
+
+assistant_id = "asst_ohl8sJLMkCw6K6xN0iUrFuwJ"
+print("Hello, how can I help you?")
+
+# Creates a thread
+message_thread = client.beta.threads.create()
+thread_id = message_thread.id
+
+while True:
+    question = input("Ask me anything: ")
+
+    client.beta.threads.messages.create(
+        thread_id=thread_id,
+        role="user",
+        content=question
+    )
+
+    # Starts a run
+    start_run = client.beta.threads.runs.create(
+        thread_id=thread_id,
+        assistant_id=assistant_id,
+        instructions="You are the assistant of Duke's Innovation Colab. Your main role is to assist students in answering their questions. Most of the students come for help to use tools like 3d printers, laser cutters, or software. All the information that you need is the document provided. You should avoid answering questions that don't have a relationship with the Colab or its facilities. If someone asks anything that is not related to the colab, you should respond that you are not able to answer their questions"
+    )
+
+    run_id = start_run.id
+
+
+
+    # Waits until gets the response
+    print("Thinking...")
+    while True:
+        run_status = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run_id)
+        if run_status.status == "completed":
+            break
+        elif run_status.status == "requires_action":
+            try: 
+                tool_call = run_status.required_action.submit_tool_outputs.tool_calls[0]
+                print(tool_call)
+                print(tool_call.function)
+                name = tool_call.function.name
+                arguments = json.loads(tool_call.function.arguments)
+
+                print("Function Name:", name)
+                print("Function Arguments:", arguments)
+
+                function_run = client.beta.threads.runs.submit_tool_outputs(
+                    thread_id=thread_id,
+                    run_id=run_id,
+                    tool_outputs=[
+                        {
+                            "tool_call_id": tool_call.id,
+                            "output": json.dumps(arguments),
+                        }
+                    ],
+                )
+
+                break
+            except: 
+                print("Tool call failed")
+                break
+        elif run_status.status == "failed":
+            print("Run failed:", run_status.last_error)
+            break
+        time.sleep(2)  
+
+    # Retrives the messages
+    messages = client.beta.threads.messages.list(
+        thread_id=thread_id
+    )
+
+    # Prints the messages
+    for i in reversed(range(0,2)):
+        try:
+            message = messages.data[i]
+            role = message.role
+            for content in message.content:
+                if content.type == 'text':
+                    response = content.text.value 
+                    print(f'\n{role}: {response}')
+        except: 
+            print("No return message")
+    print("\n")
+
+
+
