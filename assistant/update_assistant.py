f907158a979c528f992f5203f7b104742c9cfba4
project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227 <project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227@gitlab.oit.duke.edu>
2024-02-27 10:34:41 -0500

    finished update_assistant.py

---
+++ assistant/update_assistant.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+import os
+client = OpenAI()
+
+# knowledge_base = client.files.create(
+#     file=open("./formatted.pdf", "rb"),
+#     purpose='assistants'
+# )
+
+# This is a test to create an assistant from python. 
+# Don't run this program several times, otherwise you'll end up creating a bunch of assistants
+# colab_assistant = client.beta.assistants.create(
+#     instructions="You are the assistant of Duke's Innovation Colab. Your main role is to assist students in answering their questions. Most of the students come for help to use tools like 3d printers, laser cutters, or software. All the information that you need is the document provided. Anything else, you don't know. You should avoid answering questions that don't have a relationship with the Colab or its facilities. If someone asks anything that is not related to the colab, you should respond that you are not able to answer their questions",
+#     name="Colab Assistant",
+#     tools=[{"type": "retrieval"}],
+#     model="gpt-3.5-turbo-0125",
+# )
+# print(colab_assistant)
+
+assistant_id = os.environ.get("ASSISTANT_ID")
+
+my_updated_assistant = client.beta.assistants.update(
+  assistant_id,
+  instructions="You are the assistant of Duke's Innovation Colab. Your main role is to assist students in answering their questions. Most of the students come for help to use tools like 3d printers, laser cutters, or software. All the information that you need is the document provided. Anything else, you don't know. You should avoid answering questions that don't have a relationship with the Colab or its facilities. If someone asks anything that is not related to the colab, you should respond that you are not able to answer their questions",
+  name="Colab Assistant",
+  tools=[{"type": "retrieval"}, 
+         {"type": "function", "function": {
+      "name": "get_current_worker", 
+      "description": "When asked about 'who is available at the Colab?' or if a person requests help to use hardware tools, get the name of the current worker that is passed as an array. If the parameter passed is empty, respond with 'No worker is available right now'"
+      }},
+        {"type": "function", "function": {
+      "name": "get_current_student_devs", 
+      "description": "When asked about 'which student developer is available at the Colab?' or if a person requests help with software, get the name of the current worker and expertise. If the parameter passed is empty, respond with 'No worker is available right now'"
+      }}],
+  model="gpt-3.5-turbo-0125",
+)
+
+print(my_updated_assistant)
