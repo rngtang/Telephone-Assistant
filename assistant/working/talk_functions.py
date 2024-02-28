319b57b636f65159dc8cd7a4c542a2d06be77652
project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227 <project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227@gitlab.oit.duke.edu>
2024-02-28 09:48:20 -0500

    changing some stuff from the tts

--- assistant/working/talk_functions.py
+++ assistant/working/talk_functions.py
@@ -132,8 +132,9 @@ def main():
                         response = content.text.value 
                         print(f'\n{role}: {response}')
                         # problem: doesn't seem to say the first question from the user
-                        speech_synthesizer.speak_text_async(role).get()
-                        speech_synthesizer.speak_text_async(response).get()
+                        if(role == "assistant"):
+                        # speech_synthesizer.speak_text_async(role).get()
+                            speech_synthesizer.speak_text_async(response).get()
             except: 
                 print("No return message")
         print("\n")
