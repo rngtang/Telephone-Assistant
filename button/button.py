1cffed06ac920da2c4dfe873d89a86acf44b3caf
project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227 <project_38186_bot_0ebf1cb0e7da6e3b62aa95d929f4d227@gitlab.oit.duke.edu>
2024-04-08 15:33:51 -0400

    testing default library gpiozero, changed venv

---
+++ button/button.py
@@ -0,0 +1,20 @@
+from gpiozero import Button 
+# default library from pi, changed venv settings
+from signal import pause
+
+button = Button(2)
+
+def button_pressed():
+    print("Button was pressed")
+
+def button_held():
+    print("Button was held")
+
+def button_released():
+    print("Button was released")
+
+button.when_pressed = button_pressed
+button.when_held = button_held
+button.when_released = button_released
+
+pause()
